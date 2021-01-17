97be3eba1c636b2175ddb51802b35090e7ba3b74
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-01-17 16:29:20 -0300

    Add books graphql

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import express from 'express'
-// import { ApolloServer } from 'apollo-server-express'
+import { ApolloServer } from 'apollo-server-express'
 
-// import { typeDefs, resolvers } from './schema'
+import { typeDefs, resolvers } from './schema'
 
-// const app = express()
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// })
-// server.applyMiddleware({ app })
+const app = express()
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.applyMiddleware({ app })
 
 const PORT = process.env.PORT ?? 3000
-const app = express()
 
 app.get('/', (_, res) => res.json({ msg: 'Hello World' }))
 
