d5ca0b288a781510715d93ac6cfa4df090038c14
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-01-17 15:25:05 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+// import { ApolloServer } from 'apollo-server-express'
+
+// import { typeDefs, resolvers } from './schema'
+
+// const app = express()
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// })
+// server.applyMiddleware({ app })
+
+const PORT = process.env.PORT ?? 3000
+const app = express()
+
+app.get('/', (_, res) => res.json({ msg: 'Hello World' }))
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Example app listening on port ${PORT}!`)
+})
